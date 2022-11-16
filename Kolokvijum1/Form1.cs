c6de6a6d821fffc8399ec764343a958416c33871
Nikoraii <kolja.st@gmail.com>
2022-11-17 00:52:52 +0100

    Add project files.

---
+++ Kolokvijum1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Kolokvijum1.Klase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Kolokvijum1
+{
+    public partial class Form1 : Form
+    {
+        protected List<Knjiga> knjige = new List<Knjiga>();
+        public int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbListaKnjiga.Items.Clear();
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            var naslov = tbNaslov.Text;
+            var autor = tbAutor.Text;
+            var godinaizdanja = tbGodinaIzdanja.Text;
+            int godinaizdanjaint = int.Parse(godinaizdanja);
+            var listazanrova = tbListaZanrova.Text;
+            var listaocena = tbListaOcena.Text;
+            //List<Knjiga> knjige = new List<Knjiga>();
+
+            if (listazanrova == string.Empty)
+            {
+                MessageBox.Show("Morate uneti barem jedan zanr!");
+            } else if(listaocena == string.Empty)
+            {
+                MessageBox.Show("Morate uneti barem jednu ocenu!");
+            }
+            else
+            {
+                string[] ocene = listaocena.Split(',');
+                int[] oceneint = new int[ocene.Length];
+                int k = 0;
+                bool d = true;
+                foreach (var ocena in ocene)
+                {
+                    var ocenaint = Convert.ToInt32(ocena);
+                    oceneint[k] = ocenaint;
+                    k++;
+                }
+                foreach (var ocenaint in oceneint)
+                {
+                    if (ocenaint > 5 || ocenaint < 1)
+                    {
+                        MessageBox.Show("Pogresna ocena!");
+                        d = false;
+                        break;
+                    }
+                }
+                if (d == true)
+                {
+                    if (naslov == string.Empty) naslov = "Nepoznat naslov";
+                    if (autor == string.Empty) autor = "Nepoznat autor";
+                    knjige.Add(new Knjiga(naslov, autor, godinaizdanjaint, listazanrova, listaocena));
+                    lbListaKnjiga.Items.Add(knjige[i]);
+                    i++;
+                    tbNaslov.Text = "";
+                    tbAutor.Text = "";
+                    tbGodinaIzdanja.Text = "";
+                    tbListaZanrova.Text = "";
+                    tbListaOcena.Text = "";
+                }
+                else if (d == false)
+                {
+                    //d = false;
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnUpisFajl_Click(object sender, EventArgs e)
+        {
+            var file = tbUpisFajl.Text;
+            var fileX = file + ".xml";
+            var putanja = $@"F:\FAKULTET\Programiranje moblinih komunikacija\Kolokvijum1\Fajlovi\Knjige\{fileX}";
+            if(file == string.Empty)
+            {
+                MessageBox.Show("Unesite ime fajla!");
+            }
+            else
+            {
+                tbNaslov.Text = "";
+                tbAutor.Text = "";
+                tbGodinaIzdanja.Text = "";
+                tbListaZanrova.Text = "";
+                tbListaOcena.Text = "";
+                using (var swriter = new StreamWriter(putanja))
+                {
+                    XmlSerializer xmlSerializerKnjige = new XmlSerializer(typeof(List<Knjiga>));
+                    xmlSerializerKnjige.Serialize(swriter, knjige);
+                }
+                tbUpisFajl.Text = "";
+                lbListaKnjiga.Items.Clear();
+                MessageBox.Show("Uspesno ste uneli listu u fajl!");
+            }
+        }
+
+        private void btnUcitajFajl_Click(object sender, EventArgs e)
+        {
+            lbListaKnjiga.Items.Clear();
+            var file = tbUpisFajl.Text;
+            var fileX = file + ".xml";
+            var putanja = $@"F:\FAKULTET\Programiranje moblinih komunikacija\Kolokvijum1\Fajlovi\Knjige\{fileX}";
+            if (file == string.Empty)
+            {
+                MessageBox.Show("Unesite ime fajla!");
+            }
+            else
+            {
+                using (var sreader = new StreamReader(putanja))
+                {
+                    XmlSerializer xmlSerializerKnjige = new XmlSerializer(typeof(List<Knjiga>));
+                    knjige = (List<Knjiga>)xmlSerializerKnjige.Deserialize(sreader);
+                    foreach (var knjiga in knjige)
+                    {
+                        lbListaKnjiga.Items.Add(knjiga);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kolokvijum1/Klase/Knjiga.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokvijum1.Klase
+{
+    [Serializable]
+    public class Knjiga : ISerializable
+    {
+        public static int id = 1;
+        private int idd;
+        public int Id
+        {
+            get
+            {
+                return idd;
+            }
+            set
+            {
+                idd = value;
+            }
+        }
+        public string Naslov { get; set; }
+        public string Autor { get; set; }
+        public int GodinaIzdanja { get; set; }
+        public string Zanrovi { get; set; }
+        public string Ocene { get; set; }
+
+        public Knjiga(string naslov, string autor, int godinaizdanja, string zanrovi, string ocene)
+        {
+            idd = id++;
+            Naslov = naslov;
+            Autor = autor;
+            GodinaIzdanja = godinaizdanja;
+            Zanrovi = zanrovi;
+            Ocene = ocene;
+        }
+
+        public Knjiga() : this("", "", 0, "", "") { }
+
+        public double ProsecnaOcena()
+        {
+            string[] ocene = Ocene.Split(',');
+            double br_ocena = 0;
+            double zbir_ocena = 0;
+            foreach (var ocena in ocene)
+            {
+                var ocenaint = int.Parse(ocena);
+                br_ocena += 1;
+                zbir_ocena += ocenaint;
+            }
+            return Math.Round((zbir_ocena / br_ocena), 2);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Naslov} {Autor}, {GodinaIzdanja}. | Zanrovi: {Zanrovi}, Prosecna ocena: {ProsecnaOcena()}";
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Naslov", Naslov);
+            info.AddValue("Autor", Autor);
+            info.AddValue("GodinaIzdanja", GodinaIzdanja);
+            info.AddValue("Zanrovi", Zanrovi);
+            info.AddValue("Ocene", Ocene);
+        }
+
+        public Knjiga(SerializationInfo info, StreamingContext context)
+        {
+            Naslov = (string)info.GetValue("Naslov", typeof(string));
+            Autor = (string)info.GetValue("Autor", typeof(int));
+            GodinaIzdanja = (int)info.GetValue("GodinaIzdanja", typeof(int));
+            Zanrovi = (string)info.GetValue("Zanrovi", typeof(string));
+            Ocene = (string)info.GetValue("Ocene", typeof(string));
+        }
+    }
+}
